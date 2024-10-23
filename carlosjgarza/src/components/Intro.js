288fd51a6e46a37106aa63a2f8d6a195b04d0db5
Carlos Garza <128347858+JulJulien@users.noreply.github.com>
2024-10-23 21:52:54 +0000

    reformatted intro animation. Navbar Colors applied. Ready for launch!

--- carlosjgarza/src/components/Intro.js
+++ carlosjgarza/src/components/Intro.js
@@ -3,19 +3,20 @@ import "../styles/Intro.css";
 import Typed from "typed.js";
 import { useEffect, useRef } from "react";
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import "../styles/Global.css";
 
 
+const Intro = () => {
+    const el = useRef(null);
 
-const Intro = () =>{
-        const el = useRef(null);
-
-        useEffect(() => {
-            if (el.current) {
+    useEffect(() => {
+        if (el.current) {
             const typed = new Typed(el.current, {
-                strings: ["<span class='intro-name'> Welcome to my Website! I'm Carlos.</span>"],
-                typeSpeed: 100,
-                startDelay: 300,
-                showCursor: true, 
+                
+                strings: ["Carlos Garza :D"], // Text to type
+                typeSpeed: 200,
+                startDelay: 600,
+                showCursor: true,
                 cursorChar: "|",
                 contentType: 'html',
             });
@@ -23,24 +24,21 @@ const Intro = () =>{
             return () => {
                 typed.destroy();
             };
-          }
-        }, []);
+        }
+    }, []);
 
-        
-        return (
-            <div id="intro">
-                <span ref={el} className="intro-title"></span>
-                <div className='intro-subtitle'>text here</div>
-                <div className='intro-desc'>text here</div>
-                <a
-                    href="mailto:cjulgarza@gmail.com"
-                    className='intro-contact'
-                >
-                    <MailOutlineIcon style={{ fontSize: 30, color: 'navy'}} />
-                    {" Say hi! "}
-                </a>
-            </div>
-        );
+    return (
+        <div id="intro" style={{ fontWeight: 'bold', fontFamily: 'Raleway' }}>
+            <h1 className="intro-title">Welcome to my Website !!! I'm</h1>
+            <span ref={el} className="intro-name" style={{ color: 'var(--myblue)' }}></span> {/* Typing animation */}
+            <div className='intro-subtitle'></div>
+            <div className='intro-desc'></div>
+            <a href="mailto:cjulgarza@gmail.com" className='intro-contact'>
+                <MailOutlineIcon style={{ fontSize: 30, color: 'navy'}} />
+                {" Say hi! "}
+            </a>
+        </div>
+    );
 }
 
 export default Intro;
